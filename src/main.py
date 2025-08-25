ea7f4713cdbed178b7d24edf5faa19453c83a9ed
Kevin Reas <76408202+PaarkG@users.noreply.github.com>
2025-08-24 22:09:18 -0400

    discord webhook integration

--- src/main.py
+++ src/main.py
@@ -1,23 +1,45 @@
 import cv2
 import zxingcpp
 import numpy as np
+import requests
+import json
 
-database = {}
+webhook_url = "https://discord.com/api/webhooks/1409354480423534592/T_AiUvXiW0Go7d4MQAV1tGZKHoYCFAJ0TusVWWtUTQSmd8sBm2CVwwhjgLa4TtjVg-tO"
+database = {
+    "901049842": "Kevin"
+}
 
-capture = cv2.VideoCapture(0)
+image = cv2.imread("src/testcode3.jpeg")
+id = zxingcpp.read_barcodes(image)[0].text
+text = "unknown user scanned"
 
-while True:
-    ret, frame = capture.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.filter2D(frame, -1, np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]]))
+if id in database:
+    text = database[id] + " just signed in"
 
-    cv2.imshow("dumbass image", frame)
-    barcodes = zxingcpp.read_barcodes(frame)
+data = {
+    "id": "1409354480423534592",
+    "type": 1,
+    "content": text
+}
 
-    for barcode in barcodes:
-        if barcode.text in database:
-            print(database[barcode.text])
-        else:
-            database[barcode.text] = str(input("What is your name?"))
+data_json = json.dumps(data)
 
-    cv2.waitKey(1)
+requests.post(webhook_url, data)
+
+# capture = cv2.VideoCapture(0)
+
+# while True:
+#     ret, frame = capture.read()
+#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     frame = cv2.filter2D(frame, -1, np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]]))
+
+#     cv2.imshow("dumbass image", frame)
+#     barcodes = zxingcpp.read_barcodes(frame)
+
+#     for barcode in barcodes:
+#         if barcode.text in database:
+#             print(database[barcode.text])
+#         else:
+#             database[barcode.text] = str(input("What is your name?"))
+
+#     cv2.waitKey(1)
