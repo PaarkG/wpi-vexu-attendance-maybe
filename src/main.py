18980ddf22d4c6f4fd4936590bfdeff7079f4af6
Kevin Reas <76408202+PaarkG@users.noreply.github.com>
2025-08-24 22:23:01 -0400

    setup basic discord message

--- src/main.py
+++ src/main.py
@@ -4,42 +4,47 @@ import numpy as np
 import requests
 import json
 
-webhook_url = "https://discord.com/api/webhooks/1409354480423534592/T_AiUvXiW0Go7d4MQAV1tGZKHoYCFAJ0TusVWWtUTQSmd8sBm2CVwwhjgLa4TtjVg-tO"
+WEBHOOK_URL = "https://discord.com/api/webhooks/1409354480423534592/T_AiUvXiW0Go7d4MQAV1tGZKHoYCFAJ0TusVWWtUTQSmd8sBm2CVwwhjgLa4TtjVg-tO"
+
 database = {
-    "901049842": "Kevin"
+    "901049842": {"name": "Kevin", "present": False}
 }
 
-image = cv2.imread("src/testcode3.jpeg")
-id = zxingcpp.read_barcodes(image)[0].text
-text = "unknown user scanned"
+def send_message_to_discord(message_text):
+    data = {
+        "id": "1409354480423534592",
+        "type": 1,
+        "content": message_text
+    }
 
-if id in database:
-    text = database[id] + " just signed in"
+    data_json = json.dumps(data)
+    requests.post(WEBHOOK_URL, data)
 
-data = {
-    "id": "1409354480423534592",
-    "type": 1,
-    "content": text
-}
+def check_image_for_barcodes(image):
+    return zxingcpp.read_barcodes(image)
 
-data_json = json.dumps(data)
+def get_message_from_id(id):
+    person_info = database[id]
+    name = person_info["name"]
+    present = person_info["present"]
+    person_info["present"] = not present
+   
+    if present:
+        return name + " just left the workshop!"
+    else:
+        return name + " just entered the workshop!"
 
-requests.post(webhook_url, data)
+capture = cv2.VideoCapture(0)
 
-# capture = cv2.VideoCapture(0)
+while True:
+    # ret, frame = capture.read()
+    frame = cv2.imread("src/testcode3.jpeg")
 
-# while True:
-#     ret, frame = capture.read()
-#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     frame = cv2.filter2D(frame, -1, np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]]))
+    # cv2.imshow("dumbass image", frame)
+    barcodes = check_image_for_barcodes(frame)
 
-#     cv2.imshow("dumbass image", frame)
-#     barcodes = zxingcpp.read_barcodes(frame)
+    for barcode in barcodes:
+        if barcode.text in database:
+            send_message_to_discord(get_message_from_id(barcode.text))
 
-#     for barcode in barcodes:
-#         if barcode.text in database:
-#             print(database[barcode.text])
-#         else:
-#             database[barcode.text] = str(input("What is your name?"))
-
-#     cv2.waitKey(1)
+    cv2.waitKey(1)
